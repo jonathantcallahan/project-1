d45b7930d4c77824d54d18a8a029dd5da4b4b5a2
unknown <jtc7537@gmail.com>
2018-01-24 20:36:37 -0500

    saved user data to firebase

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+var config = {
+    apiKey: "AIzaSyBbGCe-jyKFshGLnmOdwHzIEOX7bIWaLgw",
+    authDomain: "project-5f04c.firebaseapp.com",
+    databaseURL: "https://project-5f04c.firebaseio.com",
+    projectId: "project-5f04c",
+    storageBucket: "",
+    messagingSenderId: "260716117815"
+  };
 
+  firebase.initializeApp(config);
+  var userStorage = firebase.database().ref("user-storage")
+
+  userStorage.on("child_added",function(snapshot){
+        console.log(snapshot.val())
+    },
+    function(errData){
+        console.log("Unable to retreive data")
+    }
+)
 
 document.onkeydown = function(event){
     if(event.which === 13){
@@ -6,6 +24,15 @@ document.onkeydown = function(event){
         var userName = $("#name-input").val()
         var userDobDay = $("#dob-input-day").val()
         var userDobMonth = $("#dob-input-month").val()
+        var userDobYear = $("#dob-input-year").val()
+        
+        userStorage.push({
+            name: userName,
+            dobDay: userDobDay,
+            dobMonth: userDobMonth,
+            dobYear: userDobYear
+        })
+
         var queryUrl = "http://history.muffinlabs.com/date/" + userDobMonth + "/" + userDobDay
         $.ajax({
             url: queryUrl,
