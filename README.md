2f793c449866935b524e4717f593b328ef4b7e31
stoversa <stoversa@gmail.com>
2018-01-30 21:22:37 -0500

    User-specific buttons

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-# project-1
-Group 5 Project 1
+![alt text](https://stoversa.github.io/project-1/helloworld_logo.png "Hello World Logo")
 
-The Narcissist App is an application that utilizes multiple APIs to deliver information about a user's date of birth in history, as well as the meaning of their name. Future plans for this application include a user-specific dashboard, horoscope, and horoscope-based compatibility information.
+# Narcisist
+
+## Purpose
+Narcisist is a web-based application that utilizes multiple APIs to deliver vanity information based on a user's birth-date and name input. Future plans for this application include a user-specific dashboard, horoscope, and horoscope-based compatibility information.
+
+### Using This Code
+
+To customize your own application, swap your own Firebase project in the config.js file. Do note that you'll also need to update your project's authentication settings to match those used by the application, or change the appilcation settings to suit your needs.
+
+### APIs Utilized
+
+1. [Today in History](http://history.muffinlabs.com/ "Today in History")
+2. [Words API](https://www.wordsapi.com/ "Words API")
+3. [Numbers API](http://numbersapi.com/ "Numbers API")
+
+### Other Technologies Used
+
+1. [Firebase (Google)](https://firebase.google.com/ "Firebase (Google)") - For authentication and database storage.
+2. [CORS Everywhere](https://github.com/Rob--W/cors-anywhere/ "CORS Everywhere") - To resolve CORS issues.
+
+### Issues
+1. CORS compatibility issues
+2. Firebase Authentication
+
+
+### Winter 2018 Team Members
+* Jonathan Callahan
+* Erick Grissom
+* Zach Harmon
+* Sam Stover
--- script.js
+++ script.js
@@ -1,62 +1,115 @@
 /********** Firebase initialization: If forking, add your credentials to config const in config.js    ***********/
 firebase.initializeApp(config);
-var userStorage = firebase.database().ref("user-storage")
+var userStorage = firebase.database().ref("user-storage/notloggedin")
 
 
-/*
 // The following code is for firebase authentication/login
 var provider = new firebase.auth.GithubAuthProvider();
-/*
-ui.start('#firebaseui-auth-container', {
-    signInOptions =[
-        // List of OAuth providers supported.
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+
+// FirebaseUI config.
+var uiConfig = {
+    callbacks: {
+        signInSuccess: function (currentUser, credential, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+        },
+        uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+    signInSuccessUrl: 'index.html',
+    signInOptions: [
+        {
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            scopes: [
+                'https://www.googleapis.com/auth/plus.login'
+            ],
+            customParameters: {
+                // Forces account selection even when one account
+                // is available.
+                prompt: 'select_account'
+            }
+        },
+        // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GithubAuthProvider.PROVIDER_ID
     ],
-    // Other config options...
-});
+    // Terms of service url.
+    tosUrl: '<your-tos-url>'
+};
 
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
 
- initApp = function() {
-        firebase.auth().onAuthStateChanged(function(user) {
-          if (user) {
+initApp = function () {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
             // User is signed in.
+            $("#firebaseui-auth-container").css("display", "none");
             var displayName = user.displayName;
             var email = user.email;
             var emailVerified = user.emailVerified;
             var photoURL = user.photoURL;
             var uid = user.uid;
+            app.uid = uid;
+            userStorage = firebase.database().ref("user-storage/" + app.uid);
+            console.log(app.uid);
+            app.buttonListener();
             var phoneNumber = user.phoneNumber;
             var providerData = user.providerData;
-            user.getIdToken().then(function(accessToken) {
-              document.getElementById('sign-in-status').textContent = 'Signed in';
-              document.getElementById('sign-in').textContent = 'Sign out';
-              document.getElementById('account-details').textContent = JSON.stringify({
-                displayName: displayName,
-                email: email,
-                emailVerified: emailVerified,
-                phoneNumber: phoneNumber,
-                photoURL: photoURL,
-                uid: uid,
-                accessToken: accessToken,
-                providerData: providerData
-              }, null, '  ');
+            user.getIdToken().then(function (accessToken) {
+                document.getElementById('sign-in-status').textContent = 'Signed in';
+                document.getElementById('sign-in').textContent = 'Sign out';
+                document.getElementById('account-details').textContent = JSON.stringify({
+                    displayName: displayName,
+                    email: email,
+                    emailVerified: emailVerified,
+                    phoneNumber: phoneNumber,
+                    photoURL: photoURL,
+                    uid: uid,
+                    accessToken: accessToken,
+                    providerData: providerData
+                }, null, '  ');
             });
-          } else {
+        } else {
             // User is signed out.
             document.getElementById('sign-in-status').textContent = 'Signed out';
             document.getElementById('sign-in').textContent = 'Sign in';
             document.getElementById('account-details').textContent = 'null';
-          }
-        }, function(error) {
-          console.log(error);
-        });
-      };
+        }
+    }, function (error) {
+        console.log(error);
+    });
+};
 
-      window.addEventListener('load', function() {
-        initApp()
+window.addEventListener('load', function () {
+    initApp()
 });
-*/
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function () {
+        // Existing and future Auth states are now persisted in the current
+        // session only. Closing the window would clear any existing state even
+        // if a user forgets to sign out.
+        // ...
+        // New sign-in will be persisted with session persistence.
+        return firebase.auth().signInWithEmailAndPassword(email, password);
+    })
+    .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    });
+
+var app = {
+    isRunning: false,
+    fullMessage: ""
+};
+//Place for our API calls
 /***** object for our API calls *******/
 var api = {
     callNameAPI: function () {
@@ -69,7 +122,7 @@ var api = {
             beforeSend: function (xhr) { xhr.setRequestHeader('X-Mashape-Key', 'KTvKMGaySOmsh75NGO7T8aR3MBbwp1rfNdIjsnwdXomPepANNE') }
         }).done(function (response) {
             var nameObj = response;
-            if (nameObj.results){
+            if (nameObj.results) {
                 var definition = nameObj.results[0]["definition"];
                 app.textTwo = definition;
             }
@@ -99,7 +152,7 @@ var api = {
                 app.text = app.text[1];
             }
             //Typing animation
-            if (app.typeAnimationTimeout != ""){
+            if (app.typeAnimationTimeout != "") {
                 clearTimeout(app.typeAnimationTimeout);
                 $("#results-container").empty();
             };
@@ -108,14 +161,14 @@ var api = {
             app.typeAnimation();
         })
     },
-    callNumbers: function() {
+    callNumbers: function () {
         app.textThree = "";
         $.ajax({
             url: "https://cors-anywhere.herokuapp.com/" + "http://numbersapi.com/" + app.userDobYear + "/year?fragment&json",
             method: "GET"
         }).done(function (response) {
-            if (response){
-            var obj = response;
+            if (response) {
+                var obj = response;
                 app.textThree = obj.text;
             }
             else (console.log("No Numbers Response"));
@@ -123,9 +176,10 @@ var api = {
     }
 }; //end API object
 /***** app object stores application functions and variables *******/
-var app = { 
+var app = {
 
     //variables
+    uid: "",
     userName: "",
     userDob: "", //user's full date of birth, taken from UI
     userDobDay: "", //parsed day from Dob
@@ -138,11 +192,11 @@ var app = {
     fullMessage: "", //this variable is currently just used in typeAnimation(); takes the text from an API call with some additioanl explanation and prints it to the UI
     typeAnimationTimeout: "", //timeout for our typeAnimationTimeoutFunction
     letterCount: 0, //this value allows us to increment the typeAnimation function
-    
+
     //animates page with info from first API, then checks for additional APIs
     typeAnimation: function () {
         if (app.letterCount === app.fullMessage.length) {
-            if (app.textTwoAdded === false){ //if we have received a response from Words API
+            if (app.textTwoAdded === false) { //if we have received a response from Words API
                 app.addSecondText(); //add its text to our results using this function
             };
         }
@@ -158,10 +212,10 @@ var app = {
         var speed = 50;
         app.typeAnimationTimeout = setTimeout(app.typeAnimation, speed);
     },
-    addSecondText: function (){
+    addSecondText: function () {
         app.letterCount = 0;
         clearTimeout(app.typeAnimationTimeout);
-        if (app.textTwo !== ""){
+        if (app.textTwo !== "") {
             console.log("business as usual")
             app.fullMessage = " Your name is most commonly associated with:  " + app.textTwo + ".";
         }
@@ -191,7 +245,7 @@ var app = {
     },
 
     //the following two functions deal with either inputting data to view content on the page, or viewing content for pre-existing users
-    addNewName: function() {
+    addNewName: function () {
         $("#results-container").empty();
         app.userName = $("#name-input").val().trim();
         app.userDob = $("#date").val();
@@ -221,21 +275,23 @@ var app = {
         api.callHistory();
         api.callNameAPI();
         api.callNumbers();
+    },
+    buttonListener: function () {
+        userStorage.on("child_added", function (snapshot) {
+            app.populateButtons(snapshot)
+        }, //pulls firebase info to the populate buttons function
+            function (errData) {
+                console.log("Unable to retreive data");
+            }
+        )
     }
 }; //end app object
 
 /************* Event listeners    *************/
-userStorage.on("child_added", function(snapshot){
-    app.populateButtons(snapshot)}, //pushes firebase info to the populate buttons function
-    function (errData) {
-        console.log("Unable to retreive data");
-    }
-)
-
 // on-click event function for when a user clcks on a pre-existing serach's button
-$(document).delegate(".user-button", "click", function (){
+$(document).delegate(".user-button", "click", function () {
     var that = $(this);
-    app.clickButton (that);
+    app.clickButton(that);
 });
 
 //delete function
@@ -248,8 +304,7 @@ $(document).delegate(".remove", "click", function () {
 
 // function when a user inputs name/dob 
 document.onkeydown = function () {
-    if (event.which === 13){
+    if (event.which === 13) {
         app.addNewName();
-    }; 
+    };
 };//adds new name/dob button and returns info about this input
-
